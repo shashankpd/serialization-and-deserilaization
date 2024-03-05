3a75b43f579ef1da813332d5435a1c9ae8576d72
shashankpd <pdshashank8@gmail.com>
2024-03-05 16:22:34 +0530

    added serialization and Deserialization

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+
+
+    public class Program
+    {
+     public int add(int a,int b)
+    {
+        int c;
+        return c = a + b;
+        
+    }
+
+
+        static void Main(string[] args)
+        {
+         Program prog = new Program();
+        var res=prog.add(3,2);
+
+        var jsonObject = new { result = res,
+            prog = prog.add(3, 7)
+        };
+
+        var json=JsonConvert.SerializeObject(jsonObject);
+        File.WriteAllText("new.json",json);
+        }
+    }
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+
+
+public class Program
+{
+    public int add(int a, int b)
+    {
+        int c;
+        return c = a + b;
+
+    }
+
+
+    static void Main(string[] args)
+    {
+        Program prog = new Program();
+        var res = prog.add(3, 2);
+
+
+        string json = File.ReadAllText("new.json");
+        var jsonObject = JsonConvert.DeserializeObject(json);
+
+        Console.WriteLine(jsonObject);
+      
+    }
+}
